bb21cce58935de6ec6982dbe1959739e24d2ce91
Your Name <alvin.onichabor05@gmail.com>
2023-01-07 03:02:53 +0100

    I'm back sikeeeeeeeeee

---
+++ index.js
@@ -0,0 +1,103 @@
+/* Data types
+Undefined, null, boolean, string, symbol, number and object
+*/
+
+//Every line in javascript should end with a semicolon ';'
+
+// A variable is a way to store and manipulate data
+/* Ways of declaring variables are
+var myName = "Alvin" "var is a way to declare a variable that is going to be used through out your program, it can be changed later....."
+let ourName = "Vin music" "let will only be used within the scope in which it is declared "
+const pi = 3.14  "const is a variable that should never change or it can't be changed"
+
+
+var a; 'this is just declaring a variable'
+var b = 2; 'this is declaring and assigning in one'
+
+console.log: it allows you to see things in the console*/
+//JavaScript is a case sensitive language and it's common practice to use carmel case
+/* Addition
+var sum = 10 + 10
+console.log(sum) 
+
+SUBTRACTION OPERATOR: -
+DIVISION OPERATOR: /
+MULTIPLICATION OPERATOR: *
+REMAINDER OPPERATOR: %
+COMPOUND ASSIGNMENT WITH AUGUMENTED ADDITION:
+var a = 3;
+
+a = a + 12;
+    OR
+a += 12; //Shorthand  
+
+COMPOUND ASSIGNMENT WITH AUGUMENTED SUBTRACTION:
+var a = 3;
+
+a = a - 12;
+    OR
+a -= 12; //Shorthand  
+
+COMPOUND ASSIGNMENT WITH AUGUMENTED MULTIPLICATION:
+var a = 3;
+
+a = a * 12;
+    OR
+a *= 12; //Shorthand  
+
+COMPOUND ASSIGNMENT WITH AUGUMENTED DIVISION:
+var a = 3;
+
+a = a / 12;
+    OR
+a /= 12; //Shorthand  
+
+
+
+INCREMENTING A NUMBER
+
+myVar = 87;
+myVar = myVar + 1;
+myVar++; //shorthand to incrementing 
+console.log(myVar)
+
+DECREMENTING A NUMBER
+myVar = 87;
+myVar = myVar - 1;
+    OR
+myVar--; // shorthand for decrementing    
+
+DECLARING STRING VARIABLES
+myFirstName = "Alvin"; The words/characters in the quotation mark are called strings literals
+
+ESCAPING LITERAL QUOTES IN STRINGS
+var myStr = "I am a "double quoted" string inside "double quotes"";
+To fix the problem above, you use the escape character which is a back slash "\"
+It is used by putting the back slash in the front of each character you wan escape
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+You can also avoid the problem by using single quotes to surround the string i.e 'I am a "double quoted" string in a "single quote"';
+
+There are things you can escape also for example
+\' single quote
+\"  double quote
+\\  backslash
+\n  newline
+\r  carriage return
+\t  tab
+\b  backspace
+\f  form feed
+
+CONCATENATION WITH PLUS OPERATOR
+myVar = "i come first"  + " I come second"
+
+CONCATENATION WITH PLUS EQUALS OPERATOR
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+CONSTRUCTING STRINGS WITH VARIABLE
+var myName = "Alvin";
+var ourStr = "Hey, my name is " + myName + ", how are you?";
+*/
+
+
+
