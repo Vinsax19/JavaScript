7a42926e25413f336ba54c924b8443c7ecce3ffe
Your Name <alvin.onichabor05@gmail.com>
2023-01-08 23:56:32 +0100

    update!!!

--- index.js
+++ index.js
@@ -97,7 +97,131 @@ ourStr += "I come second.";
 CONSTRUCTING STRINGS WITH VARIABLE
 var myName = "Alvin";
 var ourStr = "Hey, my name is " + myName + ", how are you?";
+
+APPENDING VARIABLES TO STRINGS
+var anAdjective  = "awesome!";
+var ourString = "Alvin is ";
+ourString += anAdjective;
+console.log(ourString);
+
+FINDING THE LENGTH OF A STRING
+var firstNameLength = 0
+var myFirstName = "Ada";
+firstNameLength = myFirstName.length;
+console.log(firstNameLength);
+
+BRACKET NOTATION TO FIND FIRST CHARACTER IN STRING
+var firstName = "Alvin"
+firstLetterOfFirstName = firstName[0] This is just basically a way to get the first letter of the firstname using the index number. Because javascript is a zero-index language, the first index number would be zero 
+
+STRING IMMUTABILITY: It implies that a string cannot be altered once created which means you can't change the string literals individually. But it doesn't mean the string can't be changed
+var myStr = "Jello world";
+myStr[0] = "H"; This won't work because you can alter the string literal.
+myStr = "Hello world"; This is the way to fix the problem, you have just replaced the other string with a new one by assigning it to the variable of the old string.
+
+BRACKET NOTATION TO FIND NTH CHARACTER IN STRING
+var firstName = "Alvin"
+secondLetterOfFirstName = firstName[1] It's the same concept as finding the first letter
+
+BRACKET NOTATION TO FIND THE LAST CHARACTER IN STRING
+var firstName = "Alvin"
+lastLetterOfFirstName = firstName[firstName.length - 1] This a way to find the last letter/character of a string by putting the '.length' operator in a square bracket then subtracting 1 because javascript is zero-index language
+
+BRACKET NOTATION TO FIND NTH-TO-LAST CHARACTER IN STRING
+var firstName = "Alvin"
+secondToLastLetterOfFirstName = firstName[firstName.length - 2] This a way to find the second-to-last letter/character of a string by putting the '.length' operator in a square bracket then subtracting 2, since you're looking for the second to last character
+
+WORD BLANKS
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    var result = '';
+    result += "The " + myAdjective + " " +myNoun + " " + myVerb + " to the store " + myAdverb
+    return result
+}
+
+console.log(wordBlanks("dog", "big", "ran", "quickly"));
+
+ARRAYS: It allows you to store several pieces of data in one place.
+var ourArray = ["John", 23];
+
+NESTED ARRAYS: When one of the elements in an array is another it's called a nested array or a multidimensional array.
+var myArray = [["Alvin", 20],["temi",21]];
+
+ACCESSING ARRAY DATA WITH INDEXED
+ourArray = [50,60,70];
+ourData = ourArray[0];
+console.log(ourData);
+
+MODIFY ARRY DATA WITH INDEXES 
+let ourArray = [16,17,19];
+ourArray[1] = 20;
+console.log(ourArray);
+
+ACCESS MULTI-DIMENSIONAL OR NESTED ARRAYS WITH INDEXES 
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+let myData = myArray[3][0][0]
+console.log(myData);
+
+MANIPULATE ARRAYS WITH push(): The ".push()" function is used to add data to the end of an array
+
+let ourArray = ['cassava', 'garri', 'eba'];
+ourArray.push('happy','balablue');
+console.log(ourArray); 
+
+MANIPULATE DATA WITH pop(): The ".pop()" function is used to remove the last data from an array 
+
+let ourArray = [1,2,3];
+let deletedData = ourArray.pop(); //This is if you want save the deleted data into a variable.....
+console.log(ourArray);
+
+MANIPULATE ARRAYS WITH shift(): The ".shift()" function is similar to the ".pop()" function but the difference is that the ".shift()" function is used to remove the first element in the array.
+let ourArray = ["vinsax", 20]
+ourArray.shift()
+console.log(ourArray);
+
+MANIPULATE ARRAYS WITH unshift(): The ".unshift()" function is used to add elements to the beginning of an array
+let ourArray = ["amarachi","alvin", "timi", "busayo","temi"]
+ourArray.shift();
+ourArray.pop();
+ourArray.unshift('temi');
+ourArray.push('amarachi');
+console.log(ourArray);
+
+WRITING REUSABLE CODE WITH FUNCTIONS
+Functions allows us to create reusable code. it is written in this manner: 
+
+function ourReusableFunction(){
+    console.log('Hey bro');
+}// The block of code inside the curly braces will run anytime the function is called
+
+ourReusableFunction();
+
+PASSING VALUES TO FUNCTIONS WITH ARGUMENTS
+
+function ourFunctionWithArgs(a,b) {
+    console.log(a + b);
+}
+ourFunctionWithArgs("Hey ","world")
+
+GLOBAL SCOPE AND FUNCTIONS
+Scopes refers to the visiblity of variables, variables which are defined outside of a function block have global scope. Global scope means they can be seen everywhere in your Javascript code.
+
+LOCAL SCOPE AND FUNCTIONS: Variables declared within a function as well as the function parameters have local scope. That means they are only visible from within the function
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
